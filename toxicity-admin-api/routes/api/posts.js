5c65e455173bd3d6eda21e39bcda80fc12b4aada
TyraelS <oshynkarenko1503@gmail.com>
2021-05-21 12:19:20 +0300

    added moderation endpoint

--- toxicity-admin-api/routes/api/posts.js
+++ toxicity-admin-api/routes/api/posts.js
@@ -36,7 +36,7 @@ router.get(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const { userId } = req.body;
+    const { userId, deleted, moderated } = req.body;
 
     try {
 
@@ -46,13 +46,20 @@ router.get(
 	  if(userId){
 		  user = await User.findById(userId).select('-password');
 		  if(user.role === 'admin'){
-			posts = await Post.find({status: 'open', moderation: { moderated: false }}).setOptions({limit: 15});
+			  if(deleted){
+				posts = await Post.find({status: 'deleted'}).setOptions({limit: 15}).sort({moderation: {moderationDate: -1}});
+			  }
+			  else if(moderated){
+				posts = await Post.find({moderation: { moderated: true }}).setOptions({limit: 15}).sort({moderation: {moderationDate: -1}});
+			  } else {
+				posts = await Post.find({status: 'open', moderation: { moderated: false }}).setOptions({limit: 15}).sort('-date');
+			  }
 		  } else {
-			posts = await Post.find({status: 'open'}).select('-moderation').setOptions({limit: 15});
+			posts = await Post.find({status: 'open'}).select('-moderation').setOptions({limit: 15}).sort('-date');
 		  }
 	  }
 	  else {
-		posts = await Post.find({status: 'open'}).select('-moderation').setOptions({limit: 15});
+		posts = await Post.find({status: 'open'}).select('-moderation').setOptions({limit: 15}).sort('-date');
 	  }
 
       const payload = {
@@ -72,7 +79,7 @@ router.post('/post', auth, [
     check('userId', 'userId is required')
       .not()
       .isEmpty(),
-	check('content', 'message text is required').exists()
+	check('content', 'message text is required').not().isEmpty()
   ],
   async (req, res) => {
 	const errors = validationResult(req);
@@ -124,5 +131,47 @@ router.post('/post', auth, [
     }
   }
 )
+
+router.post('/moderate', auth,
+	check('userId', 'userId is required').not().isEmpty(),
+	check('postId', 'postId is required').not().isEmpty(),
+	check('status', 'updated status is required').not().isEmpty(),
+	check('reason', 'reason is required').not().isEmpty(),
+	async (req, res) => {
+		const errors = validationResult(req);
+   		if (!errors.isEmpty()) {
+      		return res.status(400).json({ errors: errors.array() });
+    	}
+
+    	const { userId, postId, status, reason } = req.body;
+
+    	try {
+			let user;
+			let post;
+
+			user = await User.findById(userId).select('-password');
+
+			if(user.role !== 'admin'){
+				res.status(402).send('Forbidden');
+			} else {
+				post = Post.findById(postId);
+				post.moderation.moderated = true;
+				post.moderation.moderationDate = Date.now();
+				post.moderation.reason = reason;
+				post.status = status;
+
+				await post.save();
+				const payload = {
+					post
+				};
+				res.json(payload);
+			}
+		}
+		catch(err){
+			console.error(err.message);
+     		res.status(500).send('Server error');
+		}
+	}
+);
 
 module.exports = router;
